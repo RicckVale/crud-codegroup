8162a6e12b4cca7eb59a537cb047e3e8f9ca587b
RicckVale <ricckvale@hotmail.com>
2022-04-17 11:18:19 -0300

    boostrap 4 install

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="pt-br">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- CSS Boostrap 4-->
+    <link rel="stylesheet" href="src/bootstrap.min.css">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <!-- JavaScript Boostrap 4-->
+    <script src="src/jquery-3.2.1.slim.min.js"></script>
+    <script src="src/popper.js@1.12.9/dist/umd/popper.min.js"></script>
+    <script src="src/bootstrap.min.js"></script>
+  </body> 
